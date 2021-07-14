2188c24c3b609c7e48b2a24e3243d085c936fc9d
enin <enin20101@gmail.com>
2021-07-14 22:03:22 +0900

    test

--- test_page.py
+++ test_page.py
@@ -33,7 +33,7 @@ def substringCalculator(s):
 
 
 if __name__ == '__main__':
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = 'kincenvizh'
 
@@ -41,6 +41,6 @@ if __name__ == '__main__':
 
     print(result)
 
-    # fptr.write(str(result) + '\n')
+    fptr.write(str(result) + '\n')
 
-    # fptr.close()
+    fptr.close()
